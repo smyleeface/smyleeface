8345cc2d37591898089e7a46de848aadc8d50c1e
pattyr <patty.ramert@gmail.com>
2023-10-18 16:43:56 -0700

    setup pattyr.zsh-theme

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#---- Terminal themes ----#
+
+```bash
+ln -s $HOME/Projects/smyleeface/pattyr.zsh-theme $HOME/.oh-my-zsh/themes/pattyr.zsh-theme
+```
+
+read -p "In ~/.zshrc, change `ZSH_THEME=\"jonathan\"` to `ZSH_THEME=\"pattyr\"`"
