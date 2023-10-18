e5846e9fe87ad1a751a0226cd1bd73ae79b753bb
pattyr <patty.ramert@gmail.com>
2023-10-18 16:49:10 -0700

    fixed read command

--- setup.sh
+++ setup.sh
@@ -3,12 +3,11 @@
 #---- Git and GitHub ----#
 brew install git gh
 
+#---- Terminal themes ----#
+ln -s $HOME/Projects/smyleeface/pattyr.zsh-theme $HOME/.oh-my-zsh/themes/pattyr.zsh-theme
+read -p "In ~/.zshrc, change \`ZSH_THEME=\"jonathan\"\` to \`ZSH_THEME=\"pattyr\"\`. Press any key to continue..."
+
 #---- Language managers ----#
 brew install asdf
 ln -s $HOME/Projects/smyleeface/.tool-versions $HOME/.tool-versions
 asdf install
-
-#---- Terminal themes ----#
-ln -s $HOME/Projects/smyleeface/pattyr.zsh-theme $HOME/.oh-my-zsh/themes/pattyr.zsh-theme
-
-read -p "In ~/.zshrc, change `ZSH_THEME=\"jonathan\"` to `ZSH_THEME=\"pattyr\"`"
