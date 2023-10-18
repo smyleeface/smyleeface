b1e10ab9116c6e5eaf75f61e7eecc5e06cbc799c
pattyr <patty.ramert@gmail.com>
2023-10-18 16:45:38 -0700

    add .tool-versions

--- setup.sh
+++ setup.sh
@@ -5,11 +5,10 @@ brew install git gh
 
 #---- Language managers ----#
 brew install asdf
+ln -s $HOME/Projects/smyleeface/.tool-versions $HOME/.tool-versions
+asdf install
 
 #---- Terminal themes ----#
-
-```bash
 ln -s $HOME/Projects/smyleeface/pattyr.zsh-theme $HOME/.oh-my-zsh/themes/pattyr.zsh-theme
-```
 
 read -p "In ~/.zshrc, change `ZSH_THEME=\"jonathan\"` to `ZSH_THEME=\"pattyr\"`"
